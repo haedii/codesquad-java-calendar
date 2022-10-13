b71d334a0d5dc293535c60c4a13a080b9cda0c51
hwangdoyun <hwangdoyun@localhost>
2022-10-13 20:51:42 +0900

    sum class update

---
+++ src/honux/calender/sum.java
@@ -0,0 +1,23 @@
+package honux.calender;
+
+import java.util.Scanner;
+
+public class sum {
+	public static void main(String[] args) {
+		int a, b;
+		String s1, s2;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("두 수를 입력해 주세요.");
+		s1 = scanner.next();
+		s2 = scanner.next();
+
+		a = Integer.parseInt(s1);
+		b = Integer.parseInt(s2);
+
+		System.out.println(a + ", " + b);
+		System.out.println("두 수의 합은 " + (a + b) + "입니다.");
+
+		scanner.close();
+
+	}
+}
