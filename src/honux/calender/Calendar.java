a24a05198ca1fcb841d45746170951f4e5089b06
hwangdoyun <hwangdoyun@localhost>
2022-10-13 21:43:06 +0900

    recommit to store the password

--- src/honux/calender/Calendar.java
+++ src/honux/calender/Calendar.java
@@ -16,7 +16,8 @@ public class Calendar {
 		System.out.print("달을 입력하세요. : ");
 		int month = scanner.nextInt();
 		scanner.close();
-	
+		
+	    //배열 이용하기 
 		int[]maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 		System.out.printf("%d월은 %d일까지 있습니다.", month, maxDays[month - 1]);
 	}
