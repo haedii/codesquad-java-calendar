5c483463be632485cdde8db198a28fb87823a6d9
hwangdoyun <hwangdoyun@218.38.137.27>
2022-10-19 21:54:57 +0900

    modify the previous mission task code

--- src/honux/calender/Calendar.java
+++ src/honux/calender/Calendar.java
@@ -29,22 +29,22 @@ public class Calendar {
 
 	public static void main(String[] args) {
 
-		// 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
-		Scanner scanner = new Scanner(System.in);
-		Calendar cal = new Calendar();
+			Scanner scanner = new Scanner(System.in);
+			Calendar cal = new Calendar();
+	    
+			System.out.print("반복횟수를 입력하세요. : ");
+			int iterations = scanner.nextInt();
+			int[] array = new int[iterations];
 
-		System.out.print("반복횟수를 입력하세요. : ");
-		int iterations = scanner.nextInt();
-		int[] array = new int[iterations];
+			System.out.println("월을 입력하세요. : ");
+			for (int i = 0; i < iterations; i++) {
+				array[i] = scanner.nextInt();
+			}
+			for (int i = 0; i < iterations; i++) {
+				System.out.printf("%d월은 %d일까지 있습니다.\n", array[i], cal.numberOfDaysinEachMonth(array[i]));
+			}
+			
+			scanner.close();
 
-		for (int i = 0; i < iterations; i++) {
-			array[i] = scanner.nextInt();
 		}
-		for (int i = 0; i < iterations; i++) {
-			System.out.printf("%d월은 %d일까지 있습니다.\n", array[i], cal.numberOfDaysinEachMonth(array[i]));
-		}
-		
-		scanner.close();
-
-	}
 }
